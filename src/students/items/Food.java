ec27244662f92fc9a8e42a3c1e3d5b1fd0e4169e
Indr10 <131648772+Indr10@users.noreply.github.com>
2024-03-12 14:26:05 +1030

    Updated Food.java
    Made the food class inherit items and created the constructor. No comments done yet.

--- src/students/items/Food.java
+++ src/students/items/Food.java
@@ -1,6 +1,9 @@
 package students.items;
 
-public class Food {
+public class Food extends Item{
 
+	public Food(int maturationAge, int deathAge, int monetaryValue) {
+		super(maturationAge, deathAge, monetaryValue);
+	}
 
 }
