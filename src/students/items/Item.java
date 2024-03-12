d310a02f8264f719f1097a1a5748250c9bb914dd
Indr10 <131648772+Indr10@users.noreply.github.com>
2024-03-12 14:19:35 +1030

    Updated Items.java with tick, died, getValue, equals
    All the methods are completed except for toString method and the comments

--- src/students/items/Item.java
+++ src/students/items/Item.java
@@ -1,6 +1,53 @@
 package students.items;
 
 public class Item {
-
 	
+	int age;
+	int maturationAge;
+	int deathAge;
+	int monetaryValue;
+	
+	public Item(int maturationAge, int deathAge, int monetaryValue) {
+		this.age = 0;
+		this.maturationAge = maturationAge;
+		this.deathAge = deathAge;
+		this.monetaryValue = monetaryValue;
+		
+	}
+	
+	public void tick() {
+		age++;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public boolean died() {
+		return age > deathAge;
+	}
+	
+	public int getValue() {
+		
+		if (age >= maturationAge) {
+			return monetaryValue;
+		}
+		else {
+			return 0;
+		}
+	}
+	
+	public boolean equals (Object otherObj) {
+		
+		if (this.age == ((Item)otherObj).age
+				&& this.deathAge == ((Item)otherObj).deathAge
+				&& this.maturationAge == ((Item)otherObj).maturationAge
+				&& this.monetaryValue == ((Item)otherObj).monetaryValue){
+			
+			return true;			
+		}
+		else {
+			return false;
+		}
+	}
 }
