7bb580bad9dd05564202439b542d4ba4db227979
Indr10 <131648772+Indr10@users.noreply.github.com>
2024-03-12 18:23:03 +1030

    Updated Soil, UntilledSoil and Weed
    Completed the above classes with their respective methods.
    
    They pass their respective tests from the ones provided.

--- src/students/items/Soil.java
+++ src/students/items/Soil.java
@@ -1,5 +1,19 @@
 package students.items;
 
-public class Soil {
+public class Soil extends Item{
+	
+	final static int MATURATION_AGE = Integer.MAX_VALUE;
+	final static int DEATH_AGE = Integer.MAX_VALUE;
+	final static int MONETARY_VALUE = Integer.MAX_VALUE;
+	
+	public Soil() {
+		super(MATURATION_AGE, DEATH_AGE, MONETARY_VALUE);
+		
+	}
+
+	@Override
+	public String toString() {
+		return "/";
+	}
 
 }
--- src/students/items/UntilledSoil.java
+++ src/students/items/UntilledSoil.java
@@ -1,6 +1,19 @@
 package students.items;
 
-public class UntilledSoil {
+public class UntilledSoil extends Item{
 
+	final static int MATURATION_AGE = Integer.MAX_VALUE;
+	final static int DEATH_AGE = Integer.MAX_VALUE;
+	final static int MONETARY_VALUE = Integer.MAX_VALUE;
+	
+	public UntilledSoil() {
+		super(MATURATION_AGE, DEATH_AGE, MONETARY_VALUE);
+		
+	}
+
+	@Override
+	public String toString() {
+		return "/";
+	}
 
 }
