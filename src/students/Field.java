b7b0119bc9cc5e6d7b15532ec593bc764160e3c4
Indr10 <131648772+Indr10@users.noreply.github.com>
2024-03-14 22:37:40 +1030

    Updated Field
    Implemented the getValue method for returning the total value of the items in the field and getSummary emthod to return a summary of the field.

--- src/students/Field.java
+++ src/students/Field.java
@@ -4,6 +4,7 @@ import students.items.Apples;
 import students.items.Grain;
 import students.items.Item;
 import students.items.Soil;
+import students.items.UntilledSoil;
 import students.items.Weed;
 import java.util.Random;
 
@@ -48,28 +49,87 @@ public class Field {
 						field[i][j] = new Weed();
 					}
 				}
+				else if (field[i][j].died() == true) {
+					
+					field[i][j] = new UntilledSoil();
+				}
 			}			
 		}
 	}
 	
 	public void till(int row, int column) {
 		
+		field[row][column] = new Soil();
+			
+	}
+	
+//	public Object get(int row, int column) {
+//		
+//		return field[row][column].clone();
+//		
+//	}
+	
+	
+	public void plant(int row, int column) { 
 		
-		// access field location
-		// create a new instance of UntilledSoil
+		
 		
 	}
 	
-	// get method
-	
-	public void plant(int row, int column) { // one more parameter representing the thing to plant
+	public int getValue() {
 		
+		int value = 0;
 		
-		
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < width; j++) {
+				
+				value += field[i][j].getValue();
+				
+			}
+		}
+		return value;
 	}
 	
-	// getValue method
 	
-	// getSummary method
-	
+	public String getSummary() {
+		
+		int applesCount = 0;
+		int grainCount = 0;
+		int soilCount = 0;
+		int untilledCount = 0;
+		int weedCount = 0;
+		int totalPrice = 0;
+		
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < width; j++) {
+				
+				if (field[i][j] instanceof Apples) {
+					applesCount++;
+					totalPrice += field[i][j].getValue();
+				}
+				else if (field[i][j] instanceof Grain) {
+					grainCount++;
+					totalPrice += field[i][j].getValue();
+				}
+				else if (field[i][j] instanceof Soil) {
+					soilCount++;
+				}
+				else if (field[i][j] instanceof UntilledSoil) {
+					untilledCount++;
+					totalPrice += field[i][j].getValue();
+				}
+				else
+					weedCount++;
+					totalPrice += field[i][j].getValue();
+				}							
+		}		
+		return String.format("Apples:				%s%n", applesCount) + 
+			       String.format("Grain:				$%9.2f%n", grainCount) +
+			       String.format("Soil:		$%9.2f%n", soilCount) +
+			       String.format("Untilled:		$%9.2f%n", untilledCount) +
+			       String.format("Weed:			$%9.2f%n", weedCount) +
+			       String.format("For a total of:			$%9.2f%n", totalPrice ) +
+			       String.format("Total apples created:			$%9.2f%n", Apples.getGenerationCount()) +
+			       String.format("Total grain created:			$%9.2f%n", Grain.getGenerationCount());		
+	}			
 }
