9f197ea129adf310a952c466f7591bb0ee9b6a85
Indr10 <131648772+Indr10@users.noreply.github.com>
2024-03-30 21:16:14 +1030

    Comments for Soil class
    Commented the the methods, constructors and variables in Soil class.

--- src/students/items/Soil.java
+++ src/students/items/Soil.java
@@ -2,21 +2,24 @@ package students.items;
 
 public class Soil extends Item{
 	
+	// FINAL Soil attributes declaration
 	private final static int MATURATION_AGE = Integer.MAX_VALUE;
 	private final static int DEATH_AGE = Integer.MAX_VALUE;
 	private final static int MONETARY_VALUE = 0;
 	
+	// Constructor to initialize Soil with default values
 	public Soil() {
 		super(MATURATION_AGE, DEATH_AGE, MONETARY_VALUE);
 		
 	}
 
+	// Override the toString() method to show the visual representation of Soil
 	@Override
 	public String toString() {
 		return ".";
 	}
 
-
+	// Copy constructor to create a deep copy of a Soil object
 	public Soil(Item someItem) {
 		super(someItem);
 		
@@ -25,6 +28,7 @@ public class Soil extends Item{
 		this.monetaryValue = someItem.monetaryValue;
 	}
 	
+	// Override the copyCons() method to return a deep copy of the Soil object
 	@Override
 	public Soil copyCons() {
 		
