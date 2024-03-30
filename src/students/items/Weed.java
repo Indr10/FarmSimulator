9bde7e4f62e6df985a30274acf403e0c6ed5423c
Indr10 <131648772+Indr10@users.noreply.github.com>
2024-03-30 21:20:15 +1030

    Update Weed.java
    Commented the the methods, constructors and variables in Weed class.

--- src/students/items/Weed.java
+++ src/students/items/Weed.java
@@ -2,25 +2,24 @@ package students.items;
 
 public class Weed extends Item{
 	
+	// FINAL Weed attributes declaration
 	private final static int MATURATION_AGE = Integer.MAX_VALUE;
 	private final static int DEATH_AGE = Integer.MAX_VALUE;
 	private final static int MONETARY_VALUE = -1;
 	
+	// Constructor to initialize Weed with default values
 	public Weed() {
 		super(MATURATION_AGE, DEATH_AGE, MONETARY_VALUE);
 		
 	}
 
+	// Override the toString() method to show the visual representation of Weed
 	@Override
 	public String toString() {
 		return "#";
 	}
 
-	public int getMonetaryValue() {
-		
-		return 0;
-	}
-
+	// Copy constructor to create a deep copy of a Weed object
 	public Weed(Item someItem) {
 		super(someItem);
 		
@@ -30,6 +29,7 @@ public class Weed extends Item{
 		
 	}
 	
+	// Override the copyCons() method to return a deep copy of the Weed object
 	@Override
 	public Weed copyCons() {
 		
