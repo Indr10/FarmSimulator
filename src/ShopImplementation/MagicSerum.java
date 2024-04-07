63bc4c903f0fdb091d337ab721cd8fe608f50c86
Indr10 <131648772+Indr10@users.noreply.github.com>
2024-04-07 16:22:28 +0930

    Update MagicSerum.java
    JavaDoc for MagicSerum class (own implementation)

--- src/ShopImplementation/MagicSerum.java
+++ src/ShopImplementation/MagicSerum.java
@@ -3,38 +3,52 @@ package ShopImplementation;
 import students.items.Grain;
 import students.items.Apples;
 
+/**
+ * Represents a magic serum item that enhances crop properties in the shop.
+ */
 public class MagicSerum extends Shop{
 	
-	private static final String NAME = "Magic Serum";
-	private static final String EFFECT = "Maturation age of all crops have been reduced by 1\n"
-										+ "Sell value of all crops have been increased by 1 \n"
-										+ "Death age of all crops have been reduced by 1";
-	private static final int COST = 10;
+	private static final String NAME = "Magic Serum"; // Name of the magic serum
 	
-		public MagicSerum() {
+	private static final String EFFECT = "Maturation age of all crops have been reduced by 1\n"
+										+ "Sell value of all crops have been increased by 1 \n" // Effect of the magic serum on crops
+										+ "Death age of all crops have been reduced by 1"; 
+	private static final int COST = 10; // Cost of the magic serum
+	
+	/**
+	 * Constructs a new MagicSerum object with predefined name, cost, and effect.
+	 */
+	public MagicSerum() {
 		super(NAME, COST, EFFECT);
 		
 	}
-		
+	
+	/**
+	 * Applies the magic serum effect on crops by adjusting their properties.
+	 */
 	public void magicSerum() {
 		
-		
+		// Decreases maturation age of apples and grain by 1
 		Apples.setMATURATION_AGE(Apples.getMATURATION_AGE() - 1);
-		
 		Grain.setMATURATION_AGE(Grain.getMATURATION_AGE() - 1);
-	
-		Apples.setDEATH_AGE(Apples.getDEATH_AGE() - 1);
 		
+		// Decreases death age of apples and grain by 1	
+		Apples.setDEATH_AGE(Apples.getDEATH_AGE() - 1);
 		Grain.setDEATH_AGE(Grain.getDEATH_AGE() - 1);
 
+		// Increases monetary value of apples and grain by 1
 		Apples.setMONETARY_VALUE(Apples.getMONETARY_VALUE() + 1);
-		
 		Grain.setMONETARY_VALUE(Grain.getMONETARY_VALUE() + 1);
 		
+		// Prints information about the usage and effect of the magic serum		
 		System.out.println("You used " + NAME + "\n" + EFFECT+ "\n");
 
 	}
 	
+	/**
+	 * Retrieves the cost of the magic serum.
+	 * @return The cost of the magic serum.
+	 */
 	public static int getCost() {
 		return COST;
 	}
